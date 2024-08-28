daea67a9fc7e3df20814f66169f8e9a9b58c9b08
Ryanhangralim <ryanhangralim@gmail.com>
2024-08-28 10:07:59 +0800

    Add middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
+        'check.business.owner' => \App\Http\Middleware\CheckBusinessOwner::class,
+        'check.transaction.business.owner' => \App\Http\Middleware\CheckTransactionBusinessOwner::class,
     ];
 }
---
+++ app/Http/Middleware/CheckBusinessOwner.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Product;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckBusinessOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Retrieve the product ID from the route
+        $product = $request->route('product');
+    
+        if ($product && $product->business->user_id !== auth()->user()->id) {
+            return response()->json(['error' => 'You are not authorized to view this product'], 403);
+        }
+    
+        return $next($request);
+    }
+}
